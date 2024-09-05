bebfdb5f522746ea0e60a9931babf5e74e038837
yishak <yishak.110402@gmail.com>
2024-09-05 17:06:49 +0300

    first commit

--- src/components/Homepage/Hero/Hero.js
+++ src/components/Homepage/Hero/Hero.js
@@ -7,9 +7,19 @@ export default function Hero() {
         <h1>I'm Yishak Hailu</h1>
         <h4>Full Stack Developer</h4>
         <div className="hero-links">
-          <img src="./assets/Linked-in-icon.png" alt="" />
-          <img src="./assets/Instagram-icon.png" alt="" />
-          <img src="./assets/Telegram-icon.png" alt="" />
+          <a href="https://www.google.com" target="_blank" rel="noreferrer">
+            <img src="./assets/Linked-in-icon.png" alt="" />
+          </a>
+          <a href="https://www.google.com" target="_blank" rel="noreferrer">
+            <img src="./assets/Instagram-icon.png" alt="" />
+          </a>
+          <a href="https://www.google.com" target="_blank" rel="noreferrer">
+            <img src="./assets/Telegram-icon.png" alt="" />
+          </a>
+        </div>
+        <div className="hero-ctas">
+            <button>HIRE ME</button>
+            <button>MY PROJECTS</button>
         </div>
       </div>
       <div className="hero-image">
