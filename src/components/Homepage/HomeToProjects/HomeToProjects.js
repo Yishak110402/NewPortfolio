fd266012b18bdb0e6ce6a8151deb9b920d2f258c
yishak <yishak.110402@gmail.com>
2024-09-08 00:19:34 +0300

    first commit

--- src/components/Homepage/HomeToProjects/HomeToProjects.js
+++ src/components/Homepage/HomeToProjects/HomeToProjects.js
@@ -1,7 +1,21 @@
+import { useEffect, useRef, useState } from "react"
 import "./HomeToProjects.css"
 export default function HomeToProjects(){
+    const [visible, setVisible] = useState(false)
+    const homeToProjectsRef = useRef()
+    useEffect(function(){
+        const observer = new IntersectionObserver((entries)=>{
+            const entry = entries[0]
+            if(entry.isIntersecting){
+                setVisible(true)
+            }
+        },{
+            threshold:[0.9, 1]
+        })
+        observer.observe(homeToProjectsRef.current)
+    },[])
     return(
-        <div className="home-to-projects">
+        <div ref={homeToProjectsRef} className={visible ? "home-to-projects visible" : "home-to-projects"}>
             <div className="shade" />
             <h1>Check Out The Projects I've Worked On</h1>
             <button>See My Projects</button>
