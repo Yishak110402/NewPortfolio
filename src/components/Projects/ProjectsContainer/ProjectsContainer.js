50919a7c42dec8d57e264536f2b37850a3ebaa0f
yishak <yishak.110402@gmail.com>
2024-09-26 22:24:46 +0300

    first message

--- src/components/Projects/ProjectsContainer/ProjectsContainer.js
+++ src/components/Projects/ProjectsContainer/ProjectsContainer.js
@@ -1,12 +1,28 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import "./ProjectsContainer.css";
 import { GeneralContext } from "../../../hooks/GeneralContext";
 export default function ProjectsContainer() {
   const { projects } = useContext(GeneralContext);
+  const [visible, setVisible] = useState(false);
+  const projectRef = useRef();
+  useEffect(function () {
+    const observer = new IntersectionObserver(
+      function (entries) {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          setVisible(true);
+        }
+      },
+      {
+        threshold: [0, 1],
+      }
+    );
+    observer.observe(projectRef.current);
+  }, []);
   return (
-    <div className="projects-container">
+    <div className={`projects-container`}>
       {projects.map((project) => (
-        <div className="project">
+        <div ref={projectRef} className={`project ${visible ? "visible" : ""}`}>
           <div className="project-image">
             <img src={project.img} alt="" />
           </div>
--- src/components/Projects/ProjectsHeader/ProjectsHeader.js
+++ src/components/Projects/ProjectsHeader/ProjectsHeader.js
@@ -1,9 +1,28 @@
-import './ProjectsHeader.css'
+import { useEffect, useRef, useState } from "react";
+import "./ProjectsHeader.css";
 export default function ProjectsHeader() {
-    return (
-        <div className="projects-header">
-            <h1>Projects</h1>
-            <h3>Check out the projects I've worked on</h3>
-        </div>
+  const [visible, setVisible] = useState(false);
+  const headerRef = useRef();
+  useEffect(function () {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          setVisible(true);
+        }
+      },
+      {
+        threshold: [0.9, 1],
+      }
     );
+    observer.observe(headerRef.current);
+  }, []);
+  return (
+    <div
+      ref={headerRef}
+      className={`projects-header ${visible ? "visible" : ""}`}>
+      <h1>Projects</h1>
+      <h3>Check out the projects I've worked on</h3>
+    </div>
+  );
 }
