dc53edef4f14dfad54fa3e73eff7e6ef14f68a7f
yishak <yishak.110402@gmail.com>
2025-04-18 22:24:47 +0300

    first commit

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -1,8 +1,11 @@
-import "./Copyright.css"
-export default function Copyright(){
-    return(
-        <div className="copyright">
-            <p>Copyright &copy; Yishak Hailu 2024 All Rights Reserved</p>
-        </div>
-    )
+import "./Copyright.css";
+export default function Copyright() {
+  const currentYear = new Date(Date.now()).getFullYear();
+  console.log(currentYear);
+
+  return (
+    <div className="copyright">
+      <p>Copyright &copy; Yishak Hailu {currentYear} All Rights Reserved</p>
+    </div>
+  );
 }
